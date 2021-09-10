501c5d393d6998396fc69b4f79d6d8745ef76ef5
Abdullah Al Mamun <almamun00021@gmail.com>
2021-09-10 12:14:52 +0600

    day -01 added some stuffs

--- main.js
+++ main.js
@@ -6,36 +6,35 @@ var arra = [
     'shuvo',
     'manik'
 ]
-/*
-for(ar of arra){
+// use for.. of loop
+for(let ar of arra){
     console.log(ar);
 }
-*/
 
-// const ar = arra.map( newAr=> {
-//    return newAr
-// });
-// console.log(ar)
+// use map create a new array
+const ar = arra.map( newAr=> {
+   return newAr
+});
+ console.log(ar)
 
 
-/*
+
 arra.forEach(function(element) {
     console.log(element);
 });
-*/
-/*
+
 let i = 0;
 while(i<arra.length){
     console.log(arra[i]);
     i++;
 }
-*/
-/*
+
+
 for(let i = 0; i<arra.length; i++)
 {
     console.log(arra[i]);
 }
-*/
+
 // array of object
 const ob = [
       {
@@ -43,35 +42,83 @@ const ob = [
         city: "dhaka",
         dis: "Kishoreganj",
         op: "bajitpur",
-        vil: "Dhurighatatia"
+        vil: "Dhurighatatia",
+        isHome: false,
       },
       {
         name: "tutul",
         city: "jessore",
         dis: "moniranpur",
         op: "mitrpur",
-        vil: "Dhurighdgfatatia"
+        vil: "Dhurbaal",
+        isHome: true,
       },
       {
         name: "shahrukh",
         city: "rangpur",
         dis: "rangpur",
         op: "vatigahr",
-        vil: "shitaka"
+        vil: "shitaka",
+        isHome: false
+      },
+      {
+        name: "manik",
+        city: "rangpur",
+        dis: "gibanda",
+        op: "cagolbanda",
+        vil: "goat",
+        isHome: false
+      },
+      {
+        name: "shuvo",
+        city: "sylhet",
+        dis: "habiganj",
+        op: "vatigaga",
+        vil: "dolapur",
+        isHome: false
       },
   
 ]
 
-// console.log(ob[3]);
-/*
-for(item of ob){
-    console.log(item.vil);
+for(info of ob){
+  if(!info.isHome){
+    console.log(`current memebers are: ${info.name}`);
+  }
+  
 }
-*/
-ob.forEach(function(info, i, myInfo) {
-    console.log(`${i}:${info.name}`);
-    console.log(myInfo);
-  });
+
+// Higher order array methods forEach, map, filter
+ob.forEach(function(data){
+  console.log(`name: ${data.name} => city: ${data.city} => dis: ${data.dis}`);
+})
+// for in loop
+for (const key in ob) {
+  if(ob.hasOwnProperty.call(ob,key)){
+      console.log(ob[key]);
+  }
+}
+
+const arr = ob.map(newAr =>{
+  if(newAr.isHome == false){
+   console.log(`${newAr.name} barite nai`);
+  }  
+  
+})
+console.log(arr);
+
+ob.map(function(newData){
+   if(newData.isHome){
+     console.log(`${newData.name} barite`);
+   }
+})
+
+const data = ob.filter(function(newFilter){
+  return newFilter.isHome == true;
+})
+console.log(data);
+
+
+
 const myName = 'abdullah'
 console.log(myName.split(''))
 console.log(myName.toUpperCase())
@@ -92,3 +139,34 @@ displayName(12,'Hasan');
 displayName(24,'Mamun');
 displayName(25,'Khairul');
 displayName(39,'Forhad');
+
+// conditional statement
+let x = 100
+let y = 15
+if(x>y){
+  console.log(`x = ${x} is greater than y = ${y}`);
+}else{
+  console.log(`x = ${x} is not greater than y = ${y}`);
+
+}
+
+// ternary operator
+const big = x == 100 ? 'big' : 'small'
+console.log(big);
+
+color = 'blue'
+console.log(color === 'blue' ? 'Yes it is '+color : 'No! it is not '+color);
+console.log(x>y ? 'Yes '+x+' is big' : 'No');
+
+// switch case
+switch(color){
+  case 'red':
+    console.log("yes it is red");
+    break;
+  case 'blue':
+    console.log("Yes it is blue");
+    break;
+  default:
+    console.log("None of this");
+    break;
+}
